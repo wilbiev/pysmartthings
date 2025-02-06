ac376de08c9a9939e17b12ca8993f770e60e7ef1
Joostlek <joostlek@outlook.com>
2025-02-06 22:44:37 +0100

    Migrate to pyproject

--- script/generate_attributes.py
+++ script/generate_attributes.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""Generate capabilitiy constants."""
+"""Generate capability constants."""
 import re
 import sys
 
-sys.path.append(".")
 from pysmartthings.capability import ATTRIBUTES
 
 
 def main():
     """Run the script."""
-    attribs = [a for a in ATTRIBUTES]
+    attribs = list(ATTRIBUTES)
     attribs.sort()
     for a in attribs:
         print('{} = "{}"'.format(re.sub(r"([A-Z])", r"_\1", a).lower(), a))
